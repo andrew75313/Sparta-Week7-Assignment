946bddfd27b2fe91ff3f6d07da4f7d73b0f48890
Hyeonjin Kim <andrew75313@gmail.com>
2024-05-30 22:25:19 +0900

    Spring Security를 사용하지 않고 RefreshToken 구현 점검 & 코드정리

--- src/main/java/com/sparta/scheduleradvanced/controller/RefreshTokenController.java
+++ src/main/java/com/sparta/scheduleradvanced/controller/RefreshTokenController.java
@@ -3,11 +3,11 @@ package com.sparta.scheduleradvanced.controller;
 import com.sparta.scheduleradvanced.service.RefreshTokenService;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 @RequestMapping("/api")
 public class RefreshTokenController {
 
